d4b252872d7a0a3469e2ca358a4654a3731b8323
david hermann <david.stefan.hermann@gmail.com>
2023-11-15 23:38:49 +0100

    Added image availability check

--- front-end/client/src/components/RecipeImage.tsx
+++ front-end/client/src/components/RecipeImage.tsx
@@ -1,40 +1,51 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Image from "react-bootstrap/esm/Image"
 import { usePostContext } from "../context/postContext"
+import axios from "axios";
+import LoadingSpinner from "./LoadingSpinner";
 
 
 const RecipeImage = (props: { image: string; title: string }) => {
+    const [ isLoading, setIsLoading ] = useState(true)
     const [ imageExists, setImageExists ] = useState(false)
     const { replaceSpaces } = usePostContext()
     const noImage = process.env.PUBLIC_URL + ("/no-image.png")
     const url = process.env.PUBLIC_URL + ("/posts/" + props.image + ".png")
 
     // check if image exists
-    fetch(url, { method: 'HEAD' })
-        .then(res => {
-            if (res.ok) {
+    useEffect(() => {
+        // this took way too long..
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(url)
                 setImageExists(true)
-            } else {
-                setImageExists(false)
+            } catch(err) {
+                console.log(err)
             }
-            console.log("rimage: " + imageExists)
-        })
-        .catch(err => console.log("RecipeImage (fetch image): ", err.response))
+            setIsLoading(false)
+        }
+        fetchData()
+    }, [url])
+    
     
     return (
         <>
-            {
-            !imageExists ?
-            <Image
-                src={url}
-                alt={replaceSpaces(props.title)}
-                fluid
-            ></Image> :
-            <Image
-                src={noImage}
-                alt={"Image does not exist."}
-                fluid
-            ></Image>
+            {isLoading ? <LoadingSpinner></LoadingSpinner> : 
+                <>
+                {
+                imageExists ?
+                <Image
+                    src={url}
+                    alt={replaceSpaces(props.title)}
+                    fluid
+                ></Image> :
+                <Image
+                    src={noImage}
+                    alt={"Image does not exist."}
+                    fluid
+                ></Image>
+                }
+                </>
             }
         </>
     )
