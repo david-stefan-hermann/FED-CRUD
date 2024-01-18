90dfa55f43488956b8d035e0c09621d9e2784494
a-karo <xandra.karo@gmail.com>
2024-01-18 01:04:33 +0100

    added register logic, works

--- back-end/src/controllers/auth.ts
+++ back-end/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { db } from "../db.ts"
 import { ObjectId } from "mongodb"
+import bcrypt from "bcryptjs"
 
 const collectionName = "users"
 
@@ -14,4 +15,23 @@ export const logout = async (req: Request, res: Response) => {
 
 export const register = async (req: Request, res: Response) => {
     console.log("register: called")
+
+    const collection = db.collection(collectionName)
+
+    // Hash the password with bcryptjs
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(req.body.password, salt)
+
+    // Create the user
+    try {
+        await collection.insertOne({
+            username: req.body.username,
+            password: hash
+        })
+        console.log("register: user created successfully")
+        res.status(200).json("User has been created.")
+    } catch(err) {
+        console.error("register: error during user creation:", err)
+        res.status(500).json("Server error while creating the user.")
+    }
 }
