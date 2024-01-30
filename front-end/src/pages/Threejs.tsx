29d52a2d76aa966cbfb7fe740dc1b899d06f2e22
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-01-30 01:48:46 +0100

    added threejs object, drei not working

--- front-end/src/pages/Threejs.tsx
+++ front-end/src/pages/Threejs.tsx
@@ -1,14 +1,13 @@
 import React, { Suspense, useEffect, useRef, useState } from "react"
 import { Canvas } from "@react-three/fiber"
 import LoadingSpinner from "../components/LoadingSpinner"
+import { Mesh } from "three"
 
 
 const Threejs = () => {
     const [ isLoading, setIsLoading ] = useState(true)
     const [ availableHeight, setAvailableHeight ] = useState(0)
     const isMountedRef = useRef(true)
-
-    
     
 
     useEffect(() => {
@@ -30,20 +29,31 @@ const Threejs = () => {
         updateHeight()
     }, [])
 
+
+
+
+
     return (
         <>
             {isLoading ? <LoadingSpinner></LoadingSpinner> :
                 <div style={{height: availableHeight}}>
                     <p>a</p>
-                    {/*
+                    {
                     <Canvas
                         className="w-full h-screen bg-transparent"
                         camera={{near: 0.1, far: 1000}}
                     >
                         <Suspense fallback={<LoadingSpinner />}>
-                            
+
+                        <ambientLight intensity={0.1} />
+                        <directionalLight color="red" position={[0, 0, 5]} />
+                        <mesh>
+                            <boxGeometry />
+                            <meshStandardMaterial />
+                        </mesh>
+
                         </Suspense>
-                    </Canvas>*/
+                    </Canvas>
                     }
                 </div>
             }
